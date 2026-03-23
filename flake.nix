a81472a8cc755c668abb9d4d1352eef59acd7081
spiperac <spiperac@denkei.org>
2026-03-23 22:11:35 +0100

    added macos universal binary with lipo

--- flake.nix
+++ flake.nix
@@ -8,18 +8,6 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       
-      # Method 1: Use pkgsCross directly
-      mingw = nixpkgs.legacyPackages.${system}.pkgsCross.mingwW64.buildPackages;
-      
-      # Or Method 2: If you need multiple cross compilation targets
-      # mingw = (import nixpkgs {
-      #   system = "x86_64-linux";
-      #   crossSystem = {
-      #     config = "x86_64-w64-mingw32";
-      #     libc = "msvcrt";
-      #   };
-      # }).buildPackages;
-      
     in {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
@@ -35,15 +23,15 @@
           xorg.libXxf86vm
           libGL
           libGLU
-        ] ++ [ mingw.gcc mingw.binutils ];
+          glibc
+          glibc.dev
+        ]; 
 
         shellHook = ''
           export PATH=$PATH:$HOME/go/bin
           if [ ! -f "$HOME/go/bin/fyne" ]; then
             go install fyne.io/tools/cmd/fyne@latest
           fi
-          export CC=x86_64-w64-mingw32-gcc
-          export CXX=x86_64-w64-mingw32-g++
         '';
       };
     };
